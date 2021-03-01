ab9cc564374f94afe059a2609c120f0f0d68b6a4
Akshaj Mody <akshajmody@gmail.com>
2021-02-28 19:51:34 -0800

    add login component and private route

---
+++ src/components/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
+import { Link , useHistory } from 'react-router-dom';
+
+export default function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError('')
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError('Log in failed - please verify credentials')
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    //FRAGMENT
+    <>
+      <div className="w-100" style={{ maxWidth: '425px' }}>
+        <Card className="card">
+          <Card.Body>
+            <h1 className="text-center mb-3">kinvest</h1>
+            <h6 className="text-center mb-3">Sign in as an existing user</h6>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form className="form" onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Button variant="success" disabled={loading} className="w-100" type="submit">LOG IN</Button>
+            </Form>
+          </Card.Body>
+          <div className="w-100 text-center mb-3">
+            <Link className="link" to="/signup">Don't have an account? Sign up here!</Link>
+          </div>
+        </Card>
+      </div>
+    </>
+  )
+}
