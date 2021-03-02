cafc2497b46d5af56e7b8cc667351864336d92b7
Akshaj Mody <akshajmody@gmail.com>
2021-03-01 17:27:19 -0800

    add router and imports to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
     <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
       <div className='w-100' style={{ maxWidth: '400px' }}>
-        <Signup></Signup>
+        <Signup> </Signup>
       </div>
     </Container>
   )
