6b8094d86fcef771b19c8eb97ad08aae57b74d41
Akshaj Mody <akshajmody@gmail.com>
2021-03-05 01:22:12 -0800

    update private routes in app and modify signup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,25 @@ import Signup from './Signup';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContexts';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Interface from './Interface';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 
+
 function App() {
   return (
-    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
-      <div className='w-100' style={{ maxWidth: '400px' }}>
-        <Signup> </Signup>
-      </div>
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
+      {/* <div className="w-100" style={{ maxWidth: '425px' }}> */}
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Interface} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      {/* </div> */}
     </Container>
   )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,35 +1,65 @@
-import React, { useRef } from 'react';
-import { Form , Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContexts'
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Password confirmation failed')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError('Account creation failed')
+    }
+
+    setLoading(false)
+  }
 
   return (
+    //FRAGMENT
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">SIGN UP!</h2>
-          <Form>
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef}/>
-            </Form.Group>
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef}/>
-            </Form.Group>
-            <Form.Group id='password-confirm'>
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type='password' ref={passwordConfirmRef}/>
-            </Form.Group>
-            <Button className='w-100' type="submit">Sign Up!</Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? Log in
+      <div className="w-100" style={{ maxWidth: '425px' }}>
+        <Card className="card">
+          <Card.Body>
+            <h1 className="text-center mb-3">kinvest</h1>
+            <h6 className="text-center mb-3">Join today!</h6>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+              </Form.Group>
+              <Button variant="success" disabled={loading} className="w-100" type="submit">SIGN UP</Button>
+            </Form>
+          </Card.Body>
+          <div className="w-100 text-center mb-3">
+            <Link className="link" to="/login">Existing user? Log In here!</Link>
+          </div>
+        </Card>
       </div>
     </>
   )
