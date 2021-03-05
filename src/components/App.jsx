593f057de326077233dc761a5eb16eba3b8a8f8f
Akshaj Mody <akshajmody@gmail.com>
2021-03-05 02:57:38 -0800

    add splash screen, default route, and reorganize file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import Signup from './Signup';
-import { Container } from 'react-bootstrap';
-import { AuthProvider } from '../contexts/AuthContexts';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Interface from './Interface';
-import Login from './Login';
-import PrivateRoute from './PrivateRoute';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContexts";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { Splash } from "./SplashScreen/Splash";
+import Login from "./LoginSignup/Login";
+import Signup from "./LoginSignup/Signup";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "90vh" }}>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "90vh" }}
+    >
       {/* <div className="w-100" style={{ maxWidth: '425px' }}> */}
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Interface} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-            </Switch>
-          </AuthProvider>
-        </Router>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            {/* <PrivateRoute exact path="/" component={Interface} /> */}
+            <Redirect exact from="/" to="/welcome" />
+            <Route path="/welcome" component={Splash} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </AuthProvider>
+      </Router>
       {/* </div> */}
     </Container>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/SplashScreen/Splash.jsx
@@ -0,0 +1,19 @@
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+
+export const Splash = () => {
+  const history = useHistory();
+
+  return (
+    <div className="w-100" style={{ maxWidth: "425px" }}>
+      <div>PLACEHOLDER</div>
+      <div>IMAGE</div>
+      <div className="w-100 text-center mb-3">
+        <Link className="link" to="/signup">
+          Don't have an account? Sign up here!
+        </Link>
+      </div>
+    </div>
+  );
+};
