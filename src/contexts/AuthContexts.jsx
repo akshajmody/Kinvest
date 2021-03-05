9bc630e78572c1cfd370acf41584445288e00459
Akshaj Mody <akshajmody@gmail.com>
2021-03-04 20:21:08 -0800

    update auth context file

--- src/contexts/AuthContexts.jsx
+++ src/contexts/AuthContexts.jsx
@@ -1,15 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
 
-const AuthContext = React.createContext()
+const AuthContext = React.createContext();
 
-export function useAith() {
+export function useAuth() {
   return useContext(AuthContext)
 }
 
 export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  //MODULARITY - if needing to switch out of Firebase, you can login to different server here
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
+
+
+  //useeffect -> only runs once
+  //unsubscribe whenever we are done - off on state change
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout
+  }
+
   return (
-    <AuthContext.Provider>
-      {children}
+    <AuthContext.Provider value={value}>
+      {!loading && children}
     </AuthContext.Provider>
   )
 }
