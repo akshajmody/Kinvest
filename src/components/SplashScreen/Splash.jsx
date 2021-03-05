bac0feabb3f58613a9de401f247f82982380bf51
Akshaj Mody <akshajmody@gmail.com>
2021-03-05 04:15:43 -0800

    config and setup particle js for later use in splash screen

--- src/components/SplashScreen/Splash.jsx
+++ src/components/SplashScreen/Splash.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
+import ParticleBackground from "../../ParticleBG";
 
 export const Splash = () => {
   const history = useHistory();
